01f4bdb727c349746fbb2122d1520b198d044f59
Thomas Matheickal <tomatheickal@hotmail.com>
2025-05-02 12:39:14 +0100

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,27 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      ruff = {
-        cmd_env = { RUFF_TRACE = "messages" },
-        init_options = {
-          settings = {
-            logLevel = "error",
-          },
-        },
-        keys = {
-          {
-            "<leader>co",
-            LazyVim.lsp.action["source.organizeImports"],
-            desc = "Organize Imports",
-          },
-        },
-      },
-      ruff_lsp = {
-        keys = {
-          {
-            "<leader>co",
-            LazyVim.lsp.action["source.organizeImports"],
-            desc = "Organize Imports",
+      pyright = {
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic", -- or "basic" / "strict"
+            },
           },
         },
       },
