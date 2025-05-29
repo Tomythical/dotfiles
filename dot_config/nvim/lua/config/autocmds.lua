e9c1a81c7a9020fcd0c8f53c2744a8c88e26aa29
Thomas Matheickal <tomatheickal@hotmail.com>
2025-05-29 17:40:51 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    -- Set up keymaps for markdown files
+    -- The '<leader>m' group will be automatically created by which-key
+    vim.keymap.set("n", "<leader>", function() end, { desc = "Markdown" })
+    vim.keymap.set("n", "<leader>mt", "i- [ ] <Esc>", { desc = "Insert task checkbox", buffer = true })
+    vim.keymap.set("i", "<C-t>", "- [ ] ", { desc = "Insert task checkbox", buffer = true })
+  end,
+})
