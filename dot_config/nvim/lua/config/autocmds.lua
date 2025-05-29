462562e9ba104044b39fffb7c57932e33feb2109
Thomas Matheickal <tomatheickal@hotmail.com>
2025-05-29 13:27:40 +0100

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function(event)
+    local title = "vim"
+    if event.file ~= "" then
+      title = string.format("vim: %s", vim.fs.basename(event.file))
+    end
+
+    vim.fn.system({ "wezterm", "cli", "set-tab-title", title })
+  end,
+})
