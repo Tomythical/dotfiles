9a398260d5efddeb29a1d9caddf2a64e5bab8c7b
Thomas Matheickal <tomatheickal@hotmail.com>
2025-07-30 09:59:47 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua Update .dstask/.git/COMMIT_EDITMSG Update .dstask/.git/dstask/ids.bin Update .dstask/.git/index Update .dstask/.git/logs/HEAD Update .dstask/.git/logs/refs/heads/master Update .dstask/.git/refs/heads/master

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -19,40 +19,40 @@ vim.keymap.set("n", "<leader>ot", "o- [ ] ", { desc = "Create new markdown task"
 -- Surrounding words with backticks
 vim.keymap.set("n", "gs", 'ciw`<C-r>"`<Esc>', { noremap = true })
 
-local M = {}
-
-function M.open_dstask_split()
-  -- open vertical split and make it e.g. 30 columns wide
-  vim.cmd("vsplit")
-  -- open terminal in this window, running dstask
-  vim.cmd("terminal dstask")
-  -- enter terminal-mode
-  vim.cmd("startinsert")
-end
-
-local N = {}
-
--- open a horizontal split and run `dstask note <ID>`
-function N.open_dstask_note()
-  -- 1. ask for the task ID
-  local task_id = vim.fn.input("DSTask note for ID: ")
-  if task_id == "" then
-    print("Aborted: no ID given")
-    return
-  end
-
-  -- 2. split & resize to e.g. 10 lines tall
-  vim.cmd("vsplit")
-
-  -- 3. launch your shell to run dstask note and then stay at a prompt
-  local cmd = string.format("terminal dstask note %s", task_id)
-  vim.cmd(cmd)
-
-  -- 4. enter insert mode in the terminal
-  vim.cmd("startinsert")
-end
-
--- map it to <leader>d
-vim.keymap.set("n", "<leader>N", M.open_dstask_split, { desc = "Open dstask in small vertical split" })
--- map it to <leader>n
-vim.keymap.set("n", "<leader>n", N.open_dstask_note, { desc = "Open horizontal dstask note terminal" })
+-- local M = {}
+--
+-- function M.open_dstask_split()
+--   -- open vertical split and make it e.g. 30 columns wide
+--   vim.cmd("vsplit")
+--   -- open terminal in this window, running dstask
+--   vim.cmd("terminal dstask")
+--   -- enter terminal-mode
+--   vim.cmd("startinsert")
+-- end
+--
+-- local N = {}
+--
+-- -- open a horizontal split and run `dstask note <ID>`
+-- function N.open_dstask_note()
+--   -- 1. ask for the task ID
+--   local task_id = vim.fn.input("DSTask note for ID: ")
+--   if task_id == "" then
+--     print("Aborted: no ID given")
+--     return
+--   end
+--
+--   -- 2. split & resize to e.g. 10 lines tall
+--   vim.cmd("vsplit")
+--
+--   -- 3. launch your shell to run dstask note and then stay at a prompt
+--   local cmd = string.format("terminal dstask note %s", task_id)
+--   vim.cmd(cmd)
+--
+--   -- 4. enter insert mode in the terminal
+--   vim.cmd("startinsert")
+-- end
+--
+-- -- map it to <leader>d
+-- vim.keymap.set("n", "<leader>N", M.open_dstask_split, { desc = "Open dstask in small vertical split" })
+-- -- map it to <leader>n
+-- vim.keymap.set("n", "<leader>n", N.open_dstask_note, { desc = "Open horizontal dstask note terminal" })
