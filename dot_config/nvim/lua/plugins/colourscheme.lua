0b40659d200e72b6dd915a9c085429bae361e749
Thomas Matheickal <tomatheickal@hotmail.com>
2025-08-26 12:12:40 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colourscheme.lua Update .dstask/.git/COMMIT_EDITMSG Update .dstask/.git/dstask/ids.bin Update .dstask/.git/index Update .dstask/.git/logs/HEAD Update .dstask/.git/logs/refs/heads/master Update .dstask/.git/refs/heads/master

--- dot_config/nvim/lua/plugins/colourscheme.lua
+++ dot_config/nvim/lua/plugins/colourscheme.lua
@@ -1,5 +1,14 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
