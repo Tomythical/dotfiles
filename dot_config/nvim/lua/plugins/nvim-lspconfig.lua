6762ee600af34d5ddbbd233fb79e2c579b17bd37
Thomas Matheickal <tomatheickal@hotmail.com>
2025-08-29 09:56:30 +0100

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,6 +11,17 @@ return {
           },
         },
       },
+      yamlls = {
+        settings = {
+          yaml = {
+            schemas = {
+              ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.33.0-standalone-strict/all.json"] = "*.yaml",
+            },
+            validate = true,
+            completion = true,
+          },
+        },
+      },
     },
   },
 }
